6368bb04fcf90a6db6ae744571d673077d1fb989
Yuragranovsky <yuragranovsky@gmail.com>
2026-02-25 11:45:35 +0200

    MenuSelector task definition

---
+++ src/components/MenuSelector.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react'
+type Props = {
+    //TODO
+}
+const MenuSelector: FC<Props> = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MenuSelector
