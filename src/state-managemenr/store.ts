e6ea29855e82e8b8e089bfb4b8ece016edea725c
Yuragranovsky <yuragranovsky@gmail.com>
2026-02-26 16:07:36 +0200

    Fills in the TODO in the store.ts file to create a zustand store for managing game query state.

--- src/state-managemenr/store.ts
+++ src/state-managemenr/store.ts
@@ -6,7 +6,21 @@ type GameQuery = {
   parentPlatform: ParentPlatform | null;
   searchStr: string | null;
   ordering: SortOption | null;
-  //TODO
+  setGenre: (genreSlug: string | null)=> void;
+  setParentPlatform: (parentPlatform: ParentPlatform | null)=>void;
+  setSearchStr: (searchStr: string | null) => void;
+  setOrdering: (ordering: SortOption | null) => void
+
 };
-const useGameQuery = create<GameQuery>() //TODO Insert appropriate code
+const useGameQuery = create<GameQuery>(set => ({
+    genreSlug: null,
+    parentPlatform: null,
+    searchStr: null,
+    ordering: null,
+    setGenre: (genreSlug => set((state) => state.genreSlug == genreSlug ? state : {genreSlug})),
+    setParentPlatform: (parentPlatform => set(state => state.parentPlatform?.slug ==
+         parentPlatform?.slug ? state : {parentPlatform})),
+    setSearchStr: (searchStr => set(state => state.searchStr == searchStr? state : {searchStr})) ,
+    setOrdering: (ordering => set(state => state.ordering?.value == ordering?.value ? state : {ordering}))    
+})) 
 export default useGameQuery
