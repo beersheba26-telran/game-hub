27ca0d0c0caff50e2005eee8b8a849be4a9db76f
Yuragranovsky <yuragranovsky@gmail.com>
2026-03-01 14:26:53 +0200

    pagination

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,43 +1,87 @@
-import { SimpleGrid, Spinner, Text } from "@chakra-ui/react";
+import {
+  Box,
+  ButtonGroup,
+  VStack,
+  HStack,
+  IconButton,
+  Pagination,
+  SimpleGrid,
+  Spinner,
+  Text,
+} from "@chakra-ui/react";
 import GameCard from "./GameCard";
 import useGame from "@/services/hooks/useGame";
 import { FC } from "react";
+import useGameQuery from "@/state-managemenr/store";
+import { LuChevronLeft, LuChevronRight } from "react-icons/lu";
 
 const GameGrid: FC = () => {
-  const { data: games, isLoading, error } = useGame();
+  const {
+    data: games,
+    isLoading,
+    error
+  } = useGame();
 
   return (
-    <>
+    <VStack direction="column" height="80vh" overflow="hidden">
       {isLoading && <Spinner></Spinner>}
       {!!error ? (
         <Text color={"red"} fontSize="2rem" fontWeight={"bold"}>
           {error}
         </Text>
       ) : (
-        <SimpleGrid
-          width="100%"
-          columns={{
-            base: 1,
-            sm: 2,
-            md: 3,
-          }}
-          columnGap={5}
-          rowGap={12}
-          maxHeight="80vh"
-          overflowY="auto"
-          overflowX="hidden"
-          padding={{
-            base: 4,
-            sm: 1,
-            md: 5,
-          }}
-        >
-          {games?.map((g) => (
-            <GameCard key={g.id} game={g} />
-          ))}
-        </SimpleGrid>
+        <>
+          <Box flex="1" overflowY="auto" overflowX="hidden">
+            <SimpleGrid
+              width="100%"
+              columns={{
+                base: 1,
+                sm: 2,
+                md: 3,
+              }}
+              columnGap={5}
+              rowGap={12}
+              padding={{
+                base: 4,
+                sm: 1,
+                md: 5,
+              }}
+            >
+              {games?.map((g) => (
+                <GameCard key={g.id} game={g} />
+              ))}
+            </SimpleGrid>
+          </Box>
+          <HStack justifyContent="center" mt={4} pb={4}>
+            <Pagination.Root
+              count={800000}
+              pageSize={20}
+              
+              siblingCount={3}
+            >
+              <ButtonGroup variant="outline" size="sm">
+                <Pagination.PrevTrigger
+                >
+                 
+                    <LuChevronLeft></LuChevronLeft>
+                 
+                </Pagination.PrevTrigger>
+                <Pagination.Items render={(page)=> (
+                  <IconButton variant={{ base: "outline", _selected: "solid" }}>
+                    {page.value}
+                  </IconButton>
+                )}/>
+
+                
+                <Pagination.NextTrigger >
+                    <LuChevronRight></LuChevronRight>
+                </Pagination.NextTrigger>
+              </ButtonGroup>
+            </Pagination.Root>
+          </HStack>
+        </>
       )}
-    </>
+    </VStack>
   );
 };
 
